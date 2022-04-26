ed664a694ab3b1150fa8ff1808db2dedbe3e1a77
Peter H Jackson <peterhj@live.unc.edu>
2022-04-26 12:30:53 -0400

    added error endpoint

--- server.js
+++ server.js
@@ -47,17 +47,21 @@ app.use((req, res, next) => {
     status: res.statusCode,
     referer: req.headers['referer'],
     useragent: req.headers['user-agent']
-}
-const stmt = logdb.prepare('INSERT INTO accessLog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-next()
+  }
+  const stmt = logdb.prepare('INSERT INTO accessLog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+  stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+  next()
 })
 
 if (args.debug || args.d){
-app.get('/app/log/access/', (req, res, next) => {
-const stmt = logdb.prepare('SELECT * FROM accessLog').all();
-res.status(200).json(stmt);
-})
+  app.get('/app/log/access/', (req, res, next) => {
+    const stmt = logdb.prepare('SELECT * FROM accessLog').all();
+    res.status(200).json(stmt);
+  })
+
+  app.get('/app/error/', (req, res, next) => {
+    throw new Error('Error');
+  })
 }
 
 
